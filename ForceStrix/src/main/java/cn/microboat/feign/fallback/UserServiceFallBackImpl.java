1c46b814a653c11efb8e5e9221ba4e009985c09a
microboat <13955645241@163.com>
2022-07-13 16:07:24 +0800

    feat：openfeign fallback 开启降级

--- ForceStrix/src/main/java/cn/microboat/feign/fallback/UserServiceFallBackImpl.java
+++ ForceStrix/src/main/java/cn/microboat/feign/fallback/UserServiceFallBackImpl.java
@@ -1,10 +1,12 @@
 package cn.microboat.feign.fallback;
 
 import cn.microboat.feign.UserService;
+import org.springframework.stereotype.Component;
 
 /**
  * @author zhouwei
  */
+@Component
 public class UserServiceFallBackImpl implements UserService {
     @Override
     public String hi() {
